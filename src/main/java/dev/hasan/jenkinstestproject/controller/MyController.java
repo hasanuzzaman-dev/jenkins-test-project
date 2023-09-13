a6a8f98094f33df4bb840a0b3501555fc78fad56
hasan-cpl <hasanuzzaman@cloudproductionltd.com>
2023-09-13 15:01:50 +0600

    change path 2

--- src/main/java/dev/hasan/jenkinstestproject/controller/MyController.java
+++ src/main/java/dev/hasan/jenkinstestproject/controller/MyController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/jenkins-test-project")
 public class MyController {
     @GetMapping("/")
     public String home(){
